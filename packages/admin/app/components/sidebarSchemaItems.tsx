d8ce23c395e68496a94b2d7e41446841f53e930b
vallode <18506096+vallode@users.noreply.github.com>
2025-07-29 10:23:23 +0200

    Fix issues with no collections or singles

--- packages/admin/app/components/sidebarSchemaItems.tsx
+++ packages/admin/app/components/sidebarSchemaItems.tsx
@@ -2,6 +2,7 @@ import {
   useGetDataQuery,
   useGetSchemaQuery,
 } from "@/lib/store/slices/editorialApi";
+import { useMemo } from "react";
 import { NavLink } from "react-router";
 import { SidebarMenuSub, SidebarMenuSubButton } from "./ui/sidebar";
 
@@ -17,42 +18,48 @@ export default function SidebarSchemaItems({
 
   if (!schema) return null;
 
+  const items = useMemo(
+    () =>
+      Object.entries(schema).filter(([, value]) =>
+        singleton ? value.singleton : !value.singleton,
+      ),
+    [schema],
+  );
+
   return (
     <>
-      {Object.entries(schema)
-        .filter(([, value]) => (singleton ? value.singleton : !value.singleton))
-        .map(([key, value]) => {
-          return (
-            <SidebarMenuSub key={value.displayName}>
-              <NavLink
-                to={
-                  singleton
-                    ? `/admin/dashboard/${key}/default`
-                    : `/admin/dashboard/${key}`
-                }
-                className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "active" : ""
-                }
-              >
-                {({ isActive }) => (
-                  <SidebarMenuSubButton isActive={isActive} asChild>
-                    <div>
-                      <span className="inline-block overflow-hidden whitespace-nowrap text-ellipsis text-nowrap w-full">
-                        {value.displayName}
-                      </span>
+      {items.map(([key, value]) => {
+        return (
+          <SidebarMenuSub key={value.displayName}>
+            <NavLink
+              to={
+                singleton
+                  ? `/admin/dashboard/${key}/default`
+                  : `/admin/dashboard/${key}`
+              }
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+              }
+            >
+              {({ isActive }) => (
+                <SidebarMenuSubButton isActive={isActive} asChild>
+                  <div>
+                    <span className="inline-block overflow-hidden whitespace-nowrap text-ellipsis text-nowrap w-full">
+                      {value.displayName}
+                    </span>
 
-                      {!singleton && data && (
-                        <span className="text-xs text-gray-500 ml-auto pr-1">
-                          {Object.keys(data[key]).length}
-                        </span>
-                      )}
-                    </div>
-                  </SidebarMenuSubButton>
-                )}
-              </NavLink>
-            </SidebarMenuSub>
-          );
-        })}
+                    {!singleton && data?.[key] && (
+                      <span className="text-xs text-gray-500 ml-auto pr-1">
+                        {Object.keys(data[key]).length}
+                      </span>
+                    )}
+                  </div>
+                </SidebarMenuSubButton>
+              )}
+            </NavLink>
+          </SidebarMenuSub>
+        );
+      })}
     </>
   );
 }
