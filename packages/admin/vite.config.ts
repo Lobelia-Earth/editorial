b1d60d1dd815e572dfbe13c4420cc1f92cdb05b7
vallode <18506096+vallode@users.noreply.github.com>
2025-07-30 16:07:34 +0200

    Fix vite base path in production

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/admin/",
+  base: process.env.NODE_ENV === "development" ? "/admin/" : "/",
   plugins: [
     // babel({
     //   filter: /\.[jt]sx?$/,
