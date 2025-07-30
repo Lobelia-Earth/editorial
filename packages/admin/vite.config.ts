f4755f055f9b1d3bd000cd99217c25e783059d32
vallode <18506096+vallode@users.noreply.github.com>
2025-07-30 16:04:27 +0200

    Add leading slash to base path

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/admin",
+  base: "/admin/",
   plugins: [
     // babel({
     //   filter: /\.[jt]sx?$/,
