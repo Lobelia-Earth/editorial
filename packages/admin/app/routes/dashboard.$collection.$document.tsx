dcec94ab1e11cbd4c8c3d1fe9fec2a83491db48b
vallode <18506096+vallode@users.noreply.github.com>
2025-08-05 14:38:35 +0200

    Prevent 404 item from hiding form on single type

--- packages/admin/app/routes/dashboard.$collection.$document.tsx
+++ packages/admin/app/routes/dashboard.$collection.$document.tsx
@@ -18,7 +18,7 @@ export default function CollectionItemPage({ params }: Route.ComponentProps) {
 
   const isFetching = isSchemaFetching || isDataObjectFetching;
 
-  if (!schema || !item || isFetching) return null;
+  if (!schema || isFetching) return null;
 
   return (
     <div className="overflow-auto scrollbar-stable">
