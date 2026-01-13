0acc7e1d32abb32b175f5e394cf284d245540b97
aleixmarsa <aleix.marsa@gmail.com>
2026-01-13 14:58:42 +0100

    [admin] Add favicon to the app

--- packages/admin/app/root.tsx
+++ packages/admin/app/root.tsx
@@ -27,8 +27,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         <Meta />
+        <link rel="icon" type="image/ico" href="/admin/favicon.ico" />
         <Links />
       </head>
       <body
