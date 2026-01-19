418065bd01a56a47eaa59d7dbe5c515812945971
aleixmarsa <aleix.marsa@gmail.com>
2026-01-19 15:05:05 +0100

    [react] Update Vite configuration to set project as library to export

--- packages/react-adapter/vite.config.ts
+++ packages/react-adapter/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "EditorialReactAdapter",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "ReactJSXRuntime",
+        },
+      },
+    },
+    sourcemap: true,
+    cssCodeSplit: true,
+  },
+});
