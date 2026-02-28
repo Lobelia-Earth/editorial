a241b2896dc716097d5f8374c4150a02e295e248
aleixmarsa <aleix.marsa@gmail.com>
2026-02-28 23:58:03 +0100

    [admin] Add dataDiff tag to editorialApi for improved cache management

--- packages/admin/app/lib/store/slices/editorialApi.ts
+++ packages/admin/app/lib/store/slices/editorialApi.ts
@@ -30,7 +30,7 @@ export const ssrAwareBaseQuery = async (
 export const editorialApi = createApi({
   reducerPath: "editorialApi",
   baseQuery: ssrAwareBaseQuery,
-  tagTypes: ["schema", "data", "files", "config"],
+  tagTypes: ["schema", "data", "files", "config", "dataDiff"],
   endpoints: (builder) => ({
     getConfig: builder.query<EditorialConfig, void>({
       query: () => "/config",
@@ -52,6 +52,7 @@ export const editorialApi = createApi({
         method: "POST",
         body: { author },
       }),
+      invalidatesTags: () => [{ type: "dataDiff" }],
     }),
     getSchema: builder.query<EditorialSchema, void>({
       query: () => "/schema",
@@ -72,7 +73,7 @@ export const editorialApi = createApi({
     }),
     getDataDiff: builder.query<EditorialDiffResponse, void>({
       query: () => "/diff",
-      providesTags: () => [{ type: "data" }],
+      providesTags: () => [{ type: "dataDiff" }],
     }),
     getDataCount: builder.query<number, void>({
       query: () => "/data?preview=true",
@@ -178,7 +179,7 @@ export const editorialApi = createApi({
         method: "PATCH",
         body: { id, type, ...patch },
       }),
-      invalidatesTags: () => [{ type: "data" }],
+      invalidatesTags: () => [{ type: "data" }, { type: "dataDiff" }],
     }),
     createObject: builder.mutation<
       EditorialDataItem,
@@ -189,7 +190,7 @@ export const editorialApi = createApi({
         method: "PUT",
         body: { id, type, ...put },
       }),
-      invalidatesTags: () => [{ type: "data" }],
+      invalidatesTags: () => [{ type: "data" }, { type: "dataDiff" }],
     }),
     deleteObject: builder.mutation<
       void,
@@ -199,7 +200,7 @@ export const editorialApi = createApi({
         url: `/data/${type}/${id}`,
         method: "DELETE",
       }),
-      invalidatesTags: () => [{ type: "data" }],
+      invalidatesTags: () => [{ type: "data" }, { type: "dataDiff" }],
     }),
   }),
 });
